926b77bc05d1a2a9f786e50a45d99c600327cdd7
Colby Ward <linus@Elon.local>
2022-06-14 14:10:31 -0500

    first commit

---
+++ src/main/java/io/fouri/wawsup/WawsupApplication.java
@@ -0,0 +1,13 @@
+package io.fouri.wawsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WawsupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WawsupApplication.class, args);
+	}
+
+}
